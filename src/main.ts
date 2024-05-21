9b0daced0f0f0b57f09d79bc809cae9b8d415956
asos-ruairicaldwell <151652820+asos-ruairicaldwell@users.noreply.github.com>
2024-05-21 22:46:36 +0100

    support multiple yaml files

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import fs from 'fs';
 import { dirname, join } from 'path';
 import swaggerUi from 'swagger-ui-express';
 import { fileURLToPath } from 'url';
@@ -7,9 +8,16 @@ import YAML from 'yamljs';
 const app = express();
 const port = 3001;
 
-const swaggerDocument = YAML.load(join(dirname(fileURLToPath(import.meta.url)), 'schema.yaml'));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const srcDir = dirname(fileURLToPath(import.meta.url));
+
+fs.readdirSync(srcDir)
+    .filter((file) => file.endsWith('.yaml') || file.endsWith('.yml'))
+    .map((file) => ({ yaml: YAML.load(join(srcDir, file)), fileName: file.split('.')[0] }))
+    .forEach((x) => {
+        app.use(`/${x.fileName}`, swaggerUi.serveFiles(x.yaml), swaggerUi.setup(x.yaml));
+        console.log(`Swagger UI available at http://localhost:${port}/${x.fileName}`);
+    });
 
 app.listen(port, () => {
-  console.log(`Swagger UI available at http://localhost:${port}/api-docs`);
+    console.log(`Server running`);
 });
